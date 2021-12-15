0391457ba54226b3a393ff3207781bee5cec3679
Felipe Reis <ffrancis.reis@gmail.com>
2021-12-14 23:33:29 -0300

    Add contract modifier, error event
    added contract modifier and error event so that any address but the owner can not change album details

--- Album.sol
+++ Album.sol
@@ -17,18 +17,40 @@ contract Album {
     // The author of this smart contract
     string public constant contractAuthor = "ffrancis-reis";
 
+    // The owner of the current instance of this smart contract
+    address owner;
+
     // Event which will be raised anytime the current album information is updated.
     event albumEvent(
         string albumEvent_Artist,
         string albumEvent_Title,
         uint256 albumEvent_Tracks
     );
+    // Event which will be raised anytime the current album information is updated.
+    event errorEvent(string errorEvent_Description);
 
     constructor() {
         artist = "CD Projekt Red";
         albumTitle = "Cyberpunk 2077 Soundtrack";
         tracks = 19;
+        // Set the owner property of this contract instance to the initiator of this contract deployment
+        owner = msg.sender;
     } // constructor
+
+    // This function modifier ensures that the initiator of any transaction
+    //   it is attached to matches the address of the contract's owner.
+    // Use this function modifier for functions that should only
+    //   be performed by the owner of this contract instance.
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            // The initiator of this transaction is NOT the contract instance's owner!
+            emit errorEvent(
+                "Only the owner of this contract instance can perform this function!"
+            );
+        } else {
+            _;
+        } // else
+    } // modifier onlyOwner
 
     // Returns the current album information
     function getAlbum()
@@ -48,7 +70,7 @@ contract Album {
         string memory _artist,
         string memory _albumTitle,
         uint256 _tracks
-    ) public {
+    ) public onlyOwner {
         artist = _artist;
         albumTitle = _albumTitle;
         tracks = _tracks;
